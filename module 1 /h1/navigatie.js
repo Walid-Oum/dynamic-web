c55d2c93401dc51540d9b602816e8d2d4536a218
Vippy11-team <Walouma@hotmail.com>
2026-02-15 16:43:34 +0100

    hard challenge module 1

---
+++ module 1 /h1/navigatie.js
@@ -0,0 +1,105 @@
+// <nav id="mainMenu"></nav>
+// <div class="content">
+//     <h1>Hoofdstuk 1</h1>
+//     <h2>Deel 1.1</h2>
+//     <h2>Deel 1.2</h2>
+//     <h1>Hoofdstuk 2</h1>
+//     <h2>Deel 2.1</h2>
+
+
+'use strict'
+
+
+let nav = document.getElementById('mainMenu');
+let mainUl = document.createElement('ul');
+
+
+
+nav.appendChild(mainUl);
+
+
+
+let headers = document.querySelectorAll('.content h1, .content h2');
+
+
+
+
+let currentLi = null;
+let subUl = null;
+
+
+for (let header of headers) {
+
+    //tagname retourneet in uppercase (H1 en H2)
+    switch (header.tagName.toLowerCase()) {
+
+        case 'h1':
+            voegH1Toe(header);
+            break;
+        case 'h2':
+            voegH2Toe(header, currentLi);
+            break;
+
+    }
+}
+
+
+function voegH1Toe(header) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    let headername = header.textContent;
+    let newId = `${headername}`.toLowerCase().trim().replaceAll(" ", "-");
+    header.id = newId;
+    a.href = `#${newId}`;
+    a.textContent = headername;
+
+    mainUl.appendChild(li);
+    li.appendChild(a);
+    currentLi = li
+    subUl = null; 
+
+}
+
+
+function voegH2Toe(header, currentLi) {
+
+    if(!currentLi){return}
+    if (!subUl) {
+        subUl = document.createElement("ul");
+        currentLi.appendChild(subUl)
+    }
+
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    let headername = header.textContent;
+    let newId = `${headername}`.toLowerCase().trim().replaceAll(" ", "-");
+    header.id = newId;
+    a.href = `#${newId}`;
+    a.textContent = headername;
+    subUl.appendChild(li).appendChild(a);
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
